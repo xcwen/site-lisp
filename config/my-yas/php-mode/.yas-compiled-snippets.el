13ad7740f449f199db4ddff8c1d2437665499511
Jim <xcwenn@qq.com>
2015-03-31 22:34:52 +0800

    add vdoc fdoc for yas php

--- config/my-yas/php-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `php-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'php-mode
-					 '(("for" "for (${1:iss = 0}; ${2:i < N}; ${3:++i})\n{\n    $0\n}\n" "for (...; ...; ...) { ... }" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Wed Dec  3 09:41:50 2014
