2bfbd5400d1df49800f1fe0f6960f6a5fe7b2040
xcwen <xcwenn@qq.com>
2015-08-27 17:31:50 +0800

    ifx

--- config/init-indent.el
+++ config/init-indent.el
@@ -10,8 +10,6 @@
 		c-basic-offset 4
 		c-hanging-comment-ender-p nil
 		indent-tabs-mode nil)
-  ;;正常符号配对
-  (electric-pair-mode 1)
 
   ;; { 的配对 单独处理
   (make-local-variable 'skeleton-pair-alist)
@@ -20,18 +18,29 @@
   (define-key evil-insert-state-map  (kbd "{") 'skeleton-pair-insert-maybe)   
   )
 
+;;minibuffer-inactive-mode 不使用
+
+(defvar my-electic-pair-modes '(minibuffer-inactive-mode))
+
+(defun my-inhibit-electric-pair-mode (char)
+  (not (member major-mode my-electic-pair-modes)))
+
+(setq electric-pair-inhibit-predicate #'my-inhibit-electric-pair-mode)
+
+;;正常符号配对
+(electric-pair-mode 1)
 
 
 (dolist (hook (list                     ;设置用空格替代TAB的模式
-               'emacs-lisp-mode-hook
-               'lisp-mode-hook
-               'c-mode-hook
-               'c++-mode-hook
-               'js2-mode-hook
-               'sh-mode-hook
-			   'php-mode-hook
-			   'go-mode-hook
-			   'web-mode-hook
+              'emacs-lisp-mode-hook
+              'lisp-mode-hook
+              'c-mode-hook
+              'c++-mode-hook
+              'js2-mode-hook
+              'sh-mode-hook
+			  'php-mode-hook
+			  'go-mode-hook
+			  'web-mode-hook
                ))
   (add-hook hook 'c-setting ))
 
@@ -40,8 +49,8 @@
 
 			 (setq tab-width 4  indent-tabs-mode nil)
 			 ;;正常符号配对
-			 (electric-pair-mode 1)
 			 ))
+
 
 (provide 'init-indent)
 ;;; init-indent.el ends here
