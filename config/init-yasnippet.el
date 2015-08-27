072d8e8b6645e958102b186895defe33f65047d3
xcwen <xcwenn@qq.com>
2015-08-27 17:36:30 +0800

    fix

--- config/init-yasnippet.el
+++ config/init-yasnippet.el
@@ -30,26 +30,25 @@
                ))
   (add-hook hook '(lambda () (yas-minor-mode -1))))
 
-(defun delete-args-to-end ()
-  "DOCSTRING"
-  (interactive)
-  (let (start-pos end-pos)
-	(save-excursion
-      (re-search-backward "[(,]" ) 
-	  (setq start-pos (point))
-	  (when(= ?\(  (following-char))
-		  (setq start-pos (+  1 start-pos ))
-          (insert "(")
-		  )
+;; (defun delete-args-to-end ()
+;;   "DOCSTRING"
+;;   (interactive)
+;;   (let (start-pos end-pos)
+;; 	(save-excursion
+;;       (re-search-backward "[(,]" ) 
+;; 	  (setq start-pos (point))
+;; 	  (when(= ?\(  (following-char))
+;; 		  (setq start-pos (+  1 start-pos ))
+;;           (insert "(")
+;; 		  )
 	  
-      (re-search-forward "[)\n]")   
-	  (setq end-pos (1- (point)) )
-	  (kill-region start-pos end-pos )
-	  )
-	))
+;;       (re-search-forward "[)\n]")   
+;; 	  (setq end-pos (1- (point)) )
+;; 	  (kill-region start-pos end-pos )
+;; 	  )
+;; 	))
 
-;;删除多余参数
-(define-key  yas-keymap  (kbd "C-e") 'delete-args-to-end)
+;; (define-key  yas-keymap  (kbd "C-e") 'delete-args-to-end)
 
 
 (provide 'init-yasnippet)
